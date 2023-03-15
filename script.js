3a97867c8fbfb98a9e0cb7cb93c85a98ba05b459
Hemant kumar <116268777+hemant0933@users.noreply.github.com>
2023-03-15 11:54:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,61 @@
-window.promises = [promise1(), promise2(),promise3(),promise4(),promise5()];
-var output = document.getElementById('output');
-// Do not change the code above this
-// add your promises to the array `promises`
-let promise1 = new promise((resolve,reject)=>{
-	setTimeOut(()=>{
-		resolve('I am First promise')
-		output.innerHTML = 'I am First promise'
-	},1000)
-})
+// window.promises = [promise1(), promise2(),promise3(),promise4(),promise5()];
+// var output = document.getElementById('output');
+// // Do not change the code above this
+// // add your promises to the array `promises`
+// let promise1 = new promise((resolve,reject)=>{
+// 	setTimeOut(()=>{
+// 		resolve('I am First promise')
+// 		output.innerHTML = 'I am First promise'
+// 	},1000)
+// })
 
-let promise2 = new promise((resolve,reject)=>{
-	setTimeOut(()=>{
-		resolve('I am second promise')
-		output.innerHTML = 'I am second promise'
-	},1000)
-})
-let promise3 = new promise((resolve,reject)=>{
-	setTimeOut(()=>{
-		resolve('I am third promise')
-		output.innerHTML = 'I am third promise'
-	},1000)
-})
-let promise4 = new promise((resolve,reject)=>{
-	setTimeOut(()=>{
-		resolve('I am fourth promise')
-		output.innerHTML = 'I am fourth promise'
-	},1000)
-})
-let promise5 = new promise((resolve,reject)=>{
-	setTimeOut(()=>{
-		resolve('I am fifth promise')
-		output.innerHTML = 'I am fifth promise'
-	},1000)
-})
+// let promise2 = new promise((resolve,reject)=>{
+// 	setTimeOut(()=>{
+// 		resolve('I am second promise')
+// 		output.innerHTML = 'I am second promise'
+// 	},1000)
+// })
+// let promise3 = new promise((resolve,reject)=>{
+// 	setTimeOut(()=>{
+// 		resolve('I am third promise')
+// 		output.innerHTML = 'I am third promise'
+// 	},1000)
+// })
+// let promise4 = new promise((resolve,reject)=>{
+// 	setTimeOut(()=>{
+// 		resolve('I am fourth promise')
+// 		output.innerHTML = 'I am fourth promise'
+// 	},1000)
+// })
+// let promise5 = new promise((resolve,reject)=>{
+// 	setTimeOut(()=>{
+// 		resolve('I am fifth promise')
+// 		output.innerHTML = 'I am fifth promise'
+// 	},1000)
+// })
 
 
-Promise.any(promises).then((value) => {
-  console.log(value);
+// Promise.any(promises).then((value) => {
+//   console.log(value);
+// }
+const promises = [];
+
+for (let i = 0; i < 5; i++) {
+  promises.push(new Promise((resolve) => {
+    const randomTime = Math.floor(Math.random() * 5) + 1;
+    setTimeout(() => {
+      resolve(`Promise ${i + 1} resolved after ${randomTime} seconds.`);
+    }, randomTime * 1000);
+  }));
 }
-});
+
+Promise.any(promises)
+  .then((result) => {
+    const outputDiv = document.getElementById('output');
+    outputDiv.textContent = result;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+// });
